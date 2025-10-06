3b9d6e237bb26950a3c3e47c5dcd4672f0c2880d
OS-20 <insearchofdreams12@gmail.com>
2025-10-06 09:33:15 +0530

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void typeline_a(char *fname) {
+    FILE *fp = fopen(fname, "r");
+    if (!fp) { printf("File not found!\n"); return; }
+
+    char line[200];
+    while (fgets(line, sizeof(line), fp))
+        printf("%s", line);
+    fclose(fp);
+}
+
+int main() {
+    char input[100], *arg[5];
+
+    while (1) {
+        printf("myshell$ ");
+        if (!fgets(input, sizeof(input), stdin)) break;
+
+        int i = 0;
+        arg[i] = strtok(input, " \t\n");
+        if (!arg[i]) continue;
+        while ((arg[++i] = strtok(NULL, " \t\n")));
+
+        if (!strcmp(arg[0], "exit")) break;
+
+        else if (!strcmp(arg[0], "typeline") && arg[1] && !strcmp(arg[1], "-a") && arg[2])
+            typeline_a(arg[2]);
+
+        else {
+            if (fork() == 0) {
+                execvp(arg[0], arg);
+                perror("Command failed");
+                exit(1);
+            } else wait(NULL);
+        }
+    }
+    return 0;
+}
